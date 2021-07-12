4662d2c70c952d6a590101b28250587130e0b1c8
martinfzz <forget.l.martin@gmail.com>
2021-07-12 16:02:42 +0200

    Add router and Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit
-        and save to reload.
-      </p>
-    </header>
-  </div>
+  <Router>
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </main>
+  </Router>
+
 );
 
 export default App;

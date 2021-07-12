b5bd80f174d48e8b91d0696b38875770613921f8
martinfzz <forget.l.martin@gmail.com>
2021-07-12 16:11:54 +0200

    Add axios

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+import UserManager from "./user";
+import UiManager from "./ui";
+import MealsManager from "./meals";
+import QuantitiesManager from "./quantities";
+
+// eslint-disable-next-line import/prefer-default-export
+export {
+  UserManager, UiManager, MealsManager, QuantitiesManager,
+};

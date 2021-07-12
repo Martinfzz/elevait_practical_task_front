2f15fd4109bf14eea61e7215d6df5fc8a497feee
martinfzz <forget.l.martin@gmail.com>
2021-07-12 15:54:42 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit
+        and save to reload.
+      </p>
+    </header>
+  </div>
+);
+
+export default App;

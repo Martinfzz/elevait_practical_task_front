da8b9ee2025dcc7ac44045bfd96a22d01be426c3
martinfzz <forget.l.martin@gmail.com>
2021-07-14 11:36:39 +0200

    Responsive on ShowPage

--- src/components/Page/ShowPage/index.jsx
+++ src/components/Page/ShowPage/index.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 const ShowPage = ({ pageInfos }) => (
-  <Card style={{ width: '25rem' }} className="mx-auto mt-3 border border-warning">
-    <Card.Body>
-      <Card.Title className="mb-3 text-center font-weight-bold">
-        Page
-        {' '}
-        {pageInfos.pageNr}
-      </Card.Title>
-      <Card.Text>
-        {pageInfos.text}
-      </Card.Text>
-    </Card.Body>
-  </Card>
+  <div className="mx-3">
+    <Card style={{ maxWidth: '25rem' }} className="mx-auto mt-3 border border-warning">
+      <Card.Body>
+        <Card.Title className="mb-3 text-center font-weight-bold">
+          Page
+          {' '}
+          {pageInfos.pageNr}
+        </Card.Title>
+        <Card.Text>
+          {pageInfos.text}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </div>
 );
 
 export default ShowPage;
